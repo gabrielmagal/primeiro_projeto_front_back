1f41ef4d58688b6347b578927ddd2b7dadf9241a
Gabriel Almeida <41032795+gabrielmagal@users.noreply.github.com>
2022-10-10 21:46:18 -0300

    Update function.js

--- front/function.js
+++ front/function.js
@@ -1,9 +1,9 @@
 function myfunction()
 {
-	const user = document.getElementsByName("login")[0].value;
-	const pass = document.getElementsByName("password")[0].value;
+    const user = document.getElementsByName("login")[0].value;
+    const pass = document.getElementsByName("password")[0].value;
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", `http://localhost:8080/${user}/${pass}`, false );
     xmlHttp.send( null );
-	alert(xmlHttp.responseText)
+    alert(xmlHttp.responseText)
 }
